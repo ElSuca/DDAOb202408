557a76c833d681fa77e48bf5c407246ef926d314
ElSuca <springtrap370@gmail.com>
2024-11-12 20:57:01 -0300

    EL tiki

---
+++ PokerObligatorioDDA/src/inicio/Test.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package inicio;
+
+import iuGrafica.Inicio;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Test {
+    public static void main(String[]args){
+        new Inicio().setVisible(true);
+    }
+}
---
+++ PokerObligatorioDDA/src/iuGrafica/LoginJugador.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package iuGrafica;
+
+import java.awt.Frame;
+
+/**
+ *
+ * @author Usuario
+ */
+public class LoginJugador extends Login {
+    
+    public LoginJugador(Frame parent, boolean modal) {
+        super(parent, modal, "Login como JUGADOR");
+    }
+    
+}
