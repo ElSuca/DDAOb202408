39162029f6c4e665eed348d72fa489f3919c1cc0
ElSuca <springtrap370@gmail.com>
2024-11-13 11:56:29 -0300

    Agregada fachada, modelo y ui mergeados

---
+++ PokerObligatorioDDA/src/controladores/PokerController.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controladores;
+
+import observador.Observable;
+import observador.Observador;
+
+/**
+ *
+ * @author Usuario
+ */
+public class PokerController implements Observador {
+
+    @Override
+    public void actualizar(Object evento, Observable origen) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
